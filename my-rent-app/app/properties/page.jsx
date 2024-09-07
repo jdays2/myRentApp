548d568c304deb71611f2ef51aebacf5f7691dbf
Марк <lolka-pop@rambler.ru>
2024-09-07 22:43:40 +0300

    feat: add propertieCard and list

--- my-rent-app/app/properties/page.jsx
+++ my-rent-app/app/properties/page.jsx
@@ -1,9 +1,27 @@
+import { PropertyCard } from '@/components/PropertyCard';
+import properties from '@/properties.json';
+
 export const metadata = {
 	title: 'properties',
 };
 
 function PropertiesPage() {
-	return <div>PropertiesPage</div>;
+	return (
+		<section className="px-4 py-6">
+			<div className="container-xl lg:container m-auto px-4 py-6">
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+					{properties.map((e) => {
+						return (
+							<PropertyCard
+								data={e}
+								key={e._id}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default PropertiesPage;
